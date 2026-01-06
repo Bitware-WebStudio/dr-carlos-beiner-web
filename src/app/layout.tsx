1372ae4ba8f3d228f73bb56a86abfb94ab5f75ce
arozas80 <arozas@gmail.com>
2026-01-06 19:33:45 -0300

    feat: add Facebook Pixel tracking script and noscript tag to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -199,6 +199,33 @@ export default function RootLayout({
     <html lang="es" className="scroll-smooth">
       <body className={`${inter.variable} ${poppins.variable} font-sans text-neutral-800 bg-white antialiased`}>
         <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '852487927401118');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=852487927401118&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <Script
           id="ld-json"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
